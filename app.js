a57c4a619c1c7e63c040263c2acf692161890b3b
Cleber Santos <clebersantei@gmail.com>
2025-02-28 21:48:21 -0300

    versao de ocigo teste

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+//let validar = document.querySelector('input').value;
+function adicionarAmigo() {
+    let nome = document.querySelector('input').value;
+    
+    if (nome == '') {
+        alert('Por favor, insira um nome.');
+
+    } else {
+        amigos.push(nome);
+        document.querySelector('input').value = '';
+        document.querySelector('input').focus();
+        mostrarLista();
+
+        listaAmigos.push(nome);
+        console.log(listaAmigos);
+        atualizarLista();
+        Input.value = '';
+    }
+    
+    function atualizarLista(){
+        let listar = document.getElementById('listaAmigos');
+        listar.innerHTML = '';
+    }
+
+    function limparCampo() {
+    document.querySelector('input').value = '';
+    document.querySelector('input').focus();
+
+}
