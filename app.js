084efe0931865a7c78064a8b93c6d79c9543c2b4
Cleber Santos <clebersantei@gmail.com>
2025-03-04 11:10:58 -0300

    adiconei funcao add amigos

--- app.js
+++ app.js
@@ -1,31 +1,8 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 let amigos = [];
-//let validar = document.querySelector('input').value;
-function adicionarAmigo() {
-    let nome = document.querySelector('input').value;
-    
-    if (nome == '') {
-        alert('Por favor, insira um nome.');
-
-    } else {
-        amigos.push(nome);
-        document.querySelector('input').value = '';
-        document.querySelector('input').focus();
-        mostrarLista();
-
-        listaAmigos.push(nome);
-        console.log(listaAmigos);
-        atualizarLista();
-        Input.value = '';
-    }
-    
-    function atualizarLista(){
-        let listar = document.getElementById('listaAmigos');
-        listar.innerHTML = '';
-    }
-
-    function limparCampo() {
-    document.querySelector('input').value = '';
-    document.querySelector('input').focus();
-
+//acho que errei - let validar = document.querySelector('input').value;
+// acho que essa é a corrreta 
+function adicionarAmigo() { 
+let input = document.getElementById("amigo");
+let  nome = input.value.trim();
 }
